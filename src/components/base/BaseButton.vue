0173a5fc4a92abc32e5fe821d3eb4acc92f8c956
shady356 <heenriko@gmail.com>
2021-03-07 22:27:53 +0100

    feat: small adjustments to base button design

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -23,6 +23,7 @@ export default {
   color: #2be;
   border-radius: 50px 15% / 50% 40px;
   border: 4px solid #2299ee60;
+  box-shadow: 0 2px 4px #0077cc60;
   letter-spacing: 1px;
   font-size: 28px;
   font-weight: 700;
